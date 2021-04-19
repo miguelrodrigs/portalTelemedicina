3a4a3c69f4e1cc118dfcc1b01653c5f1f978e868
Miguel <miguelcazemiro@gmail.com>
2021-04-18 21:23:51 -0300

    subindo teste portal telemedicina

---
+++ venv/sex_predictor.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+#importação dos dados com pandas
+dados = pd.read_csv('test_data_CANDIDATE.csv')
+
+#Tratamento dos dados
+dados['sex'] = dados['sex'].str.upper()
+dados = dados.fillna(0)
+
+#carregando dataset nas variaveis X e y para fazer o treinamento
+X = dados.drop(columns = ['sex'])
+y = dados['sex']
+
+#treinando o modelo com os parametros encontrados com GridSearch
+model_rf = RandomForestClassifier(random_state=0, max_features='auto', n_estimators= 100, max_depth=5)
+model_rf.fit(X, y)
+
+#carregando o novo arquivo de exemplo sem a coluna sexo
+newsample = pd.read_csv('newsample.csv')
+
+#removendo os valores nulos
+newsample = newsample.fillna(0)
+
+#fazendo a predição do modelo no novo arquivo
+out = model_rf.predict(newsample)
+
+#exportando arquivo csv com a coluna sexo dos dados previstos no arquivo de exemplo
+pd.DataFrame(out).to_csv('newsample_PREDICTIONS_{Miguel_Angelo_C._R._de_Lima}.csv')
